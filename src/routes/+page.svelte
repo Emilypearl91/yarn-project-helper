2febdf19a62669f17d53c0e3ea134487dbeab2dd
Emily Pearl <Emilypearl9193@gmail.com>
2024-08-16 23:24:23 -0600

    description of website on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
 <header class="w-100 h-96 bg-[#C9E1F3] overflow-hidden">
   <img src="images/blue-knit.jpg" alt="up close light blue jumbo knit">
 </header>
+<h1 class="h1 max-w-1/2 text-center my-4">Welcome to your fiber art project helper!</h1>
+<p class="h3 max-w-1/2 m-20">This website was created to help along with the process of fiber arts projects, with keeping track of the rows you have completed or figuring out what hook/needle to use with what weight of yarn. Or if you are looking to sell your finished work, the calculator is your best friend! </p>
