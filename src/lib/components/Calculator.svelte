6d68039df38d70ab4a36fc865205c1e3ef70f704
Emily Pearl <Emilypearl9193@gmail.com>
2024-08-16 22:10:27 -0600

    added cost of materials to calculator card and added text to distinguish each input

--- src/lib/components/Calculator.svelte
+++ src/lib/components/Calculator.svelte
@@ -3,25 +3,31 @@ let total = 0
 // function to calculate the total worth of the project
 const calculate = () => {
   // amount variable to get time and pay values and multiply 
-let amount = document.getElementById('time').valueAsNumber * document.getElementById('pay').valueAsNumber;
+const amount = document.getElementById('time').valueAsNumber * document.getElementById('pay').valueAsNumber;
 // check that amount is correct
 console.log(amount);
 // gst variable to multiply the amount by 5% to get how much gst to add 
-let gst = amount * 0.05;
+const gst = amount * 0.05;
 // check that gst is correct
 console.log(gst);
+// materials variable to incorporate into the final cost
+const materials = document.getElementById('mat').valueAsNumber;
 // total value 
-total = amount + gst;
+total = amount + gst + materials;
 // check that total is correct
 console.log(total);
 };
 </script>
 
-<section class="w-96 h-96 m-auto my-28 p-2 bg-tertiary-500 rounded text-center">
+<section class="w-96 h-[400 px] m-auto my-28 p-2 bg-tertiary-500 rounded text-center">
   <h1 class="h2 text-secondary-500">Calculate how much you project is worth!</h1>
   <div class="text-center">
-  <input type="number"  id="time" placeholder="hours it took" class="text-black m-2" value="0">
-  <input type="number" id="pay" placeholder="pay per hour" class="text-black m-2" value="0">
+  <p class="text-black">Hours it took to make:</p>
+  <input type="number"  id="time" class="text-black m-2" value="0">
+  <p class="text-black">Pay per hour:</p>
+  <input type="number" id="pay" class="text-black m-2" value="0">
+  <p class="text-black">Cost of materials:</p>
+  <input type="number" id="mat" class="text-black m-2" value="0">
   <p class="text-black">tax is 5%</p>
   </div>
   <button type="submit" class="btn btn-lg variant-filled-primary m-2" on:click={calculate}> submit</button>
