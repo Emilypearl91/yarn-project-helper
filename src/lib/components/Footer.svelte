aef085549c6f44cbeb8aaed25e4c53760a19f8a6
Emily Pearl <Emilypearl9193@gmail.com>
2024-08-18 21:51:19 -0600

    footer with copyright and helpful links for fiber artists

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,8 @@
+<section class=" h-40 bg-primary-500">
+<h2 class="h2 text-center">copyright© Emily Seafoot 2024</h2>
+<p class="h4 m-2">helpful links for your next project:</p>
+<ul class="m-2">
+  <li><a href="https://www.ravelry.com/" class="h4 hover:text-tertiary-500">Ravelry</a></li>
+  <li><a href="https://www.craftyarncouncil.com/standards" class="h4 hover:text-tertiary-500">crafty yarn council: standards</a></li>
+</ul>
+</section>
