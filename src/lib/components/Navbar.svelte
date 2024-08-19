8eb3f3b7970244241703b30f055db966f1699441
Emily Pearl <Emilypearl9193@gmail.com>
2024-08-18 21:54:06 -0600

    hover to links in nav bar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -5,10 +5,10 @@
 
 <nav class="max-w h-20 bg-primary-500">
   <ul class="flex flex-row justify-end">
-    <li class="h4 m-5"><a href="/Calculator">Calculator</a></li>
-    <li class="h4 m-5"><a href="/Row-counter">Row Counter</a></li>
-    <li class="h4 m-5"><a href="/">Home</a></li>
-    <li class="h4 m-5"><LightSwitch /></li>
+    <li class="h4 m-5 hover:text-tertiary-500"><a href="/Calculator">Calculator</a></li>
+    <li class="h4 m-5 hover:text-tertiary-500"><a href="/Row-counter">Row Counter</a></li>
+    <li class="h4 m-5 hover:text-tertiary-500"><a href="/">Home</a></li>
+    <li class="h4 m-5 hover:text-tertiary-500"><LightSwitch /></li>
   
 </ul>
 </nav>
